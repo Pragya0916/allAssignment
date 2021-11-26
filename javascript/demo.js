4f6d7d789b866c9b20578230b9900618ee558336
Pragya Singh <92393235+Pragya0916@users.noreply.github.com>
2021-11-26 19:38:28 +0530

    Add files via upload

---
+++ javascript/demo.js
@@ -0,0 +1,35 @@
+// function sumOfProductOfDigits(n1, n2)
+// {let sum = 0;
+     
+  
+//     while (n1 > 0 && n2 > 0)
+//     {
+//         sum += ((n1 % 10) * (n2 % 10));
+//         n1 = Math.floor(n1/10);
+//         n2 = Math.floor(n2/10);
+//     }
+//     return sum;
+// }
+// let n1=54;
+// let n2= 2;
+// console.log(sumOfProductOfDigits(n1, n2));
+
+
+// const xyz = document.querySelector('.navbar__links');
+// console.log(xyz);
+
+
+
+// var links = document.getElementsByClassName('navbar__links');
+// console.log(Array.isArray(Array.from(links)));
+// console.log(links);
+// console.log(Array.from(links));
+// Array.from(links).forEach(function(link){
+//     console.log(link);
+// })
+
+
+
+
+var num = 12345678;
+console.log(num.toString().split(''));
